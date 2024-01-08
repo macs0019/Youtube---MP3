24b8c58183e107fb7c6f2b8440bc4d60f85a88ff
Macs0021 <carrionmarcoa@gmail.com>
2024-01-08 12:28:26 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Youtube---MP3/",
+  build: {
+    outDir: './build', // Cambia 'build' al nombre del directorio deseado
+  },
 })
